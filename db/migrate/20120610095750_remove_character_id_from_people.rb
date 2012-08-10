fda54a7bd29f7a3888ed14df3d1097eae4638960
Kai Noah Schirmer <kai@terminal21.de>
2012-08-10 22:19:45 +0200

    problematische migration entfernt

--- db/migrate/20120610095750_remove_character_id_from_people.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCharacterIdFromPeople < ActiveRecord::Migration
-  def up
-    remove_column :people, :character_id
-  end
-
-  def down
-    add_column :people, :character_id, :integer
-  end
-end
